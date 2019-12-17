d1005194cb671cfccad6a09ffdf3d668ad487dbf
kkignasiak <kkignasiak.98@gmail.com>
2019-12-17 10:35:55 +0100

    adding Company Controller

--- AppWithUsers4/Controllers/CompanyController.cs
+++ AppWithUsers4/Controllers/CompanyController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppWithUsers4.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,9 +9,150 @@ using System.Web.Http;
 
 namespace AppWithUsers4.Controllers
 {
+
     [Authorize]
+    [RoutePrefix("api/Company")]
     public class CompanyController : ApiController
     {
+        private ApplicationDbContext CompanyContext = new ApplicationDbContext();
+        
+        // GET/api/Company
+        [HttpGet]
+        public IHttpActionResult GetUsers([FromUri]PagingParameterModel paging)
+        {
+            if (paging.PageNumber <= 0)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var Companies = CompanyContext.Companies.
+                    OrderBy(u => u.Name)
+                    .AsQueryable()
+                    .Skip((paging.PageNumber - 1) * paging.PageSize)
+                    .Take(paging.PageSize).ToList();
 
+                List<CompanyViewPublicModel> Models = new List<CompanyViewPublicModel>();
+                foreach(Company Company in Companies)
+                {
+                    if(Company.IsDeleted == false)
+                    {
+                        CompanyViewPublicModel Model = new CompanyViewPublicModel();
+                        Model.Name = Company.Name;
+                        Model.Nip = Company.Nip;
+                        Model.IndustryType = Company.IndustryType;
+                        Model.Address = Company.Address;
+                        Model.City = Company.City;
+                        Model.userID = Company.userID;
+
+                        Models.Add(Model);
+                    }
+                }
+
+                return Ok(Models);
+            }
+        }
+
+        // GET /api/Company/1
+        [HttpGet]
+        public IHttpActionResult GetCompany(int Id)
+        {
+            var Company = CompanyContext.Companies.Single(c => c.Id == Id);
+
+            if (Company == null || Company.IsDeleted == true)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                CompanyViewPublicModel Model = new CompanyViewPublicModel();
+                Model.Name = Company.Name;
+                Model.Nip = Company.Nip;
+                Model.IndustryType = Company.IndustryType;
+                Model.Address = Company.Address;
+                Model.City = Company.City;
+                Model.userID = Company.userID;
+                return Ok(Model);
+            }
+            
+        }
+
+        // POST/api/Company
+        [HttpPost]
+        public IHttpActionResult CreateCompany([FromBody]CompanyAddBindingModel Model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest("invalid data.");
+            }
+            if (Model == null)
+            {
+                return BadRequest("any information about company was specified");
+            }
+            else
+            {
+                Company NewCompany = new Company();
+                NewCompany.Name = Model.Name;
+                NewCompany.Address = Model.Address;
+                NewCompany.City = Model.City;
+                NewCompany.Nip = Model.Nip;
+               NewCompany.IndustryType = Model.IndustryType;
+
+                string UserId = User.Identity.GetUserId();
+                ApplicationUser AppUser = CompanyContext.Users.Single(u => u.Id == UserId); 
+                NewCompany.userID = AppUser;
+
+                CompanyContext.Companies.Add(NewCompany);
+                CompanyContext.SaveChanges();
+                return Ok(NewCompany);
+            }
+        }
+
+        // PUT /api/Company/1
+        [HttpPut]
+        public IHttpActionResult UpdateCompany(int Id, CompanyUpdateBindingModel Model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest("invalid data.");
+            }
+
+            var Company = CompanyContext.Companies.Single(c => c.Id == Id);
+
+            if (Company == null || Company.IsDeleted == true)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                Company.Name = Model.Name ?? Company.Name;
+                Company.Nip = Model.Nip ?? Company.Nip;
+                Company.IndustryType = Model.IndustryType ?? Company.IndustryType;
+                Company.Address = Model.Address?? Company.Address;
+                Company.City = Model.City ?? Company.City;
+
+                CompanyContext.SaveChanges();
+                return Ok();
+            }
+
+        }
+
+        //DELETE /api/Company/1
+        [HttpDelete]
+
+        public IHttpActionResult DeleteCompany(int Id)
+        {
+            Company CompanyToDelete = CompanyContext.Companies.Single(c => c.Id == Id);
+
+            if (CompanyToDelete == null || CompanyToDelete.IsDeleted == true)
+            {
+                return NotFound();
+            }
+            CompanyContext.Companies.Single(c => c.Id == Id).IsDeleted = true;
+            CompanyContext.SaveChanges();
+            return Ok();
+        }
     }
 }
---
+++ AppWithUsers4/Models/CompanyBindingModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWithUsers4.Models
+{
+    public class CompanyAddBindingModel
+    {
+        public string Name { get; set; }
+        public int Nip { get; set; }
+        public Industry IndustryType { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        public int Id { get; set; }
+    }
+
+    public class CompanyUpdateBindingModel
+    {
+        public string Name { get; set; }
+        public int? Nip { get; set; }
+        public Industry IndustryType { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+
+    public class CompanyViewPublicModel
+    {
+        public string Name { get; set; }
+        public int Nip { get; set; }
+        public Industry IndustryType { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public ApplicationUser userID { get; set; }
+    }
+}
