33f274ece7ce3f670abea270f5d42228b7bd5a59
kkignasiak <kkignasiak.98@gmail.com>
2019-12-17 19:53:43 +0100

    add IndustryController

---
+++ AppWithUsers4/Controllers/IndustryController.cs
@@ -0,0 +1,42 @@
+﻿using AppWithUsers4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppWithUsers4.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Industry")]
+
+    // POST/api/Industry
+    public class IndustryController : ApiController
+    {
+        private ApplicationDbContext IndustryContext = new ApplicationDbContext();
+        [HttpPost]
+        public IHttpActionResult CreateIndustry([FromBody]Industry Industry)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest("invalid data.");
+            }
+            if (Industry == null)
+            {
+                return BadRequest("any information about Industry was specified");
+            }
+
+            else
+            {
+                Industry NewIndustry = new Industry();
+                NewIndustry.Name = Industry.Name;
+
+                IndustryContext.Industries.Add(NewIndustry);
+                IndustryContext.SaveChanges();
+
+                return Ok();
+            }
+        }
+    }
+}
