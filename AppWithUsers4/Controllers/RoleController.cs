03d74fe4fc5e06f3c41f1965902d14e4c913592d
kkignasiak <kkignasiak.98@gmail.com>
2020-01-01 13:19:51 +0100

    delete function from RoleController., wchich does not work

--- AppWithUsers4/Controllers/RoleController.cs
+++ AppWithUsers4/Controllers/RoleController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,8 +19,19 @@ namespace AppWithUsers4.Controllers
     [RoutePrefix("api/Role")]
     public class RoleController : ApiController
     {
-        private ApplicationUserManager _userManager;
         private ApplicationDbContext RoleContext = new ApplicationDbContext();
+        private ApplicationUserManager _userManager;
+
+        public RoleController()
+        {
+        }
+
+        public RoleController(ApplicationUserManager userManager,
+            ISecureDataFormat<AuthenticationTicket> accessTokenFormat)
+        {
+            UserManager = userManager;
+           // AccessTokenFormat = accessTokenFormat;
+        }
 
         public ApplicationUserManager UserManager
         {
@@ -32,6 +44,7 @@ namespace AppWithUsers4.Controllers
                 _userManager = value;
             }
         }
+
         [Route("GetAll")]
         public IHttpActionResult GetAllRoles()
         {
@@ -67,50 +80,6 @@ namespace AppWithUsers4.Controllers
                 return Ok();
             }
 
-        }
-
-        [Route("user/{id:guid}/roles")]
-        [HttpPut]
-        public async Task<IHttpActionResult> AssignRolesToUser([FromUri] string id, [FromBody] string[] rolesToAssign)
-        {
-
-            var appUser = await UserManager.FindByIdAsync(id);
-
-            if (appUser == null)
-            {
-                return NotFound();
-            }
-            var Rolestore = new RoleStore<IdentityRole>(new ApplicationDbContext());
-            var roleManager = new RoleManager<IdentityRole>(Rolestore);
-
-            var currentRoles = await UserManager.GetRolesAsync(appUser.Id);
-
-            var rolesNotExists = rolesToAssign.Except(roleManager.Roles.Select(x => x.Name)).ToArray();
-
-            if (rolesNotExists.Count() > 0)
-            {
-
-                ModelState.AddModelError("", string.Format("Roles '{0}' does not exixts in the system", string.Join(",", rolesNotExists)));
-                return BadRequest(ModelState);
-            }
-
-            IdentityResult removeResult = await UserManager.RemoveFromRolesAsync(appUser.Id, currentRoles.ToArray());
-
-            if (!removeResult.Succeeded)
-            {
-                ModelState.AddModelError("", "Failed to remove user roles");
-                return BadRequest(ModelState);
-            }
-
-            IdentityResult addResult = await UserManager.AddToRolesAsync(appUser.Id, rolesToAssign);
-
-            if (!addResult.Succeeded)
-            {
-                ModelState.AddModelError("", "Failed to add user roles");
-                return BadRequest(ModelState);
-            }
-
-            return Ok();
         }
     }
 }
